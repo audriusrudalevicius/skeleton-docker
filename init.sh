ec7b2909d3127ed47ef7fa89f65b0062db606d39
Audrius <d3trax@users.noreply.github.com>
2014-09-04 22:25:03 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 USER_PASSWORD=`pwgen -c -n -1 12`
 echo "devop password: $USER_PASSWORD"
 echo "devop password (user and mysql): $USER_PASSWORD" >> /passwords
-create-sistearth-user
+
 echo "devop:${USER_PASSWORD}" | chpasswd
 
 sleep 2s
