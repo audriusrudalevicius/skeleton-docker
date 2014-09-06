683a3dc77f4b2303ed343235e36aeeae54832748
d3trax <d3trax@gmail.com>
2014-09-06 17:10:53 +0300

    fixed message

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 USER_PASSWORD=`pwgen -c -n -1 12`
 echo "devop password: $USER_PASSWORD"
-echo "devop password (user and mysql): $USER_PASSWORD" >> /passwords
+echo "devop password user $USER_PASSWORD" >> /passwords
 
 echo "devop:${USER_PASSWORD}" | chpasswd
-
-sleep 2s
